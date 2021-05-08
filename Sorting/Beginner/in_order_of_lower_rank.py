59d3321f7e66b0f77a52ecf8ea7572684a77e068
Jinseong <aimer120@nate.com>
2021-05-08 10:25:42 +0900

    Fix: "성적이 낮은 순서로 학생 출력하기" 정렬문제 잘못된 부분 정정     - 딕셔너리 해법 삭제 -> 정렬 방법 오류 및 튜플 방식과 동일

--- Sorting/Beginner/in_order_of_lower_rank.py
+++ Sorting/Beginner/in_order_of_lower_rank.py
@@ -2,23 +2,11 @@
 
 n = int(input())
 
-# version 1 - 딕셔너리
-students = {}
-for _ in range(n):
-    student = input().split()
-    students[student[0]] = int(student[1])
-
-# 딕셔너리의 sorted는 기본적으로 value를 기준으로 정렬한 후 key값을 리스트로 리턴한다
-for name in sorted(students):
-    print(name, end=' ')
-
-
-# version 2 - 튜플
 students = []
 for _ in range(n):
     student = input().split()
     students.append((student[0], int(student[1])))
 
-# 람다식을 이용해 성적을 key값으로 하여 정렬한다
+# 람다식을 이용해 성적을 기준으로 정렬한다
 for name, rank in sorted(students, key=lambda student : student[1]):
     print(name, end=' ')
